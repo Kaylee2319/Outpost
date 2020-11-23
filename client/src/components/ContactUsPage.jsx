f133fd953586dbc493057fc320d0c4ebb9bf7f49
kaylee harding <kayleeharding23@gmail.com>
2020-11-22 23:36:02 -0500

    contact page created

--- client/src/components/ContactUsPage.jsx
+++ client/src/components/ContactUsPage.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import '../css/ContactUsPage.css';
-
+import NavBar from './NavBar';
+import Footer from './footer';
 const ContactUs = () => {
   return (
-    <div>
-      <h1>This is the Contact Page.</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="contactTitle">
+        <h1 className="contactTitle">Contact Us</h1>
+        <h3 className="contactTitle1">Have a question or feedback?</h3>
+        <h3 className="contactTitle1">Leave us a message!</h3>
+      </div>
+      <form className="contactform">
+        <div>
+          <div htmlFor="firstName">First Name:</div>
+          <input className="firstNameContact" type="text" name="name" />
+        </div>
+        <div>
+          <div htmlFor="lastName">Last Name:</div>
+          <input className="lastNameContact" type="text" name="name" />
+        </div>
+        <div>
+          <div htmlFor="message">Message:</div>
+          <textarea
+            className="messageContact"
+            name="messageContact"
+            placeholder="Questions & Feedback here..."
+          ></textarea>
+        </div>
+        <div className="sendbutton">
+          <button type="submit" className="contactSend">
+            SEND
+          </button>
+        </div>
+      </form>
+      <Footer />
+    </>
   );
 };
 
