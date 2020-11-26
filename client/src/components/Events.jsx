571262f54058009f86aada03d769530971cbe32f
philgoncalves33 <philgoncalves33@gmail.com>
2020-11-26 00:09:01 -0500

    added create event modal

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 import NavBar from './NavBar';
 import Footer from './Footer';
 import { Link } from 'react-router-dom';
+import CreateEvent from './CreateEvent';
 import '../css/EventsPage.css';
 
 const Events = () => {
+  const [modal, setModal] = useState(false);
+
+  const handleClick = (event) => {
+    setModal(true);
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
   return (
     <React.Fragment>
       <NavBar />
       <div>Home</div>
       <div>
-        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          dateClick={handleClick}
+        />
       </div>
+      <CreateEvent className="modal" show={modal} onHide={closeModal} />
       <div>
         <h4>
           <Link to="/upcomingevents">
