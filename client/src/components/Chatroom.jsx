336d18cc134d9e18f96ceb0ef14f0b62cb5cef8b
kaylee harding <kayleeharding23@gmail.com>
2020-11-27 12:30:50 -0500

    fixed chatroom changed ws to wss

--- client/src/components/Chatroom.jsx
+++ client/src/components/Chatroom.jsx
@@ -5,7 +5,7 @@ import ChatMessage from './ChatMessage';
 import NavBar from './NavBar';
 import Footer from './Footer';
 import '../css/Chat.css';
-const URL = 'wss://outpostgaming.herokuapp.com/';
+const URL = 'wss://outpostgaming.herokuapp.com/chats';
 
 class Chatroom extends Component {
   static contextType = AppContext;
@@ -14,22 +14,22 @@ class Chatroom extends Component {
     messages: [],
     avatar: {}
   };
-  ws = new WebSocket(URL, ['soap', 'wamp']);
+  wss = new WebSocket(URL, ['soap', 'wamp']);
   componentDidMount() {
-    this.ws.onopen = () => {
+    this.wss.onopen = () => {
       // on connecting, do nothing but log it to the console
       console.log('connected');
     };
-    this.ws.onmessage = (evt) => {
+    this.wss.onmessage = (evt) => {
       // on receiving a message, add it to the list of messages
       const message = JSON.parse(evt.data);
       this.addMessage(message);
     };
-    this.ws.onclose = () => {
+    this.wss.onclose = () => {
       console.log('disconnected');
       // automatically try to reconnect on connection loss
       this.setState({
-        ws: new WebSocket(URL)
+        wss: new WebSocket(URL)
       });
     };
   }
@@ -38,7 +38,7 @@ class Chatroom extends Component {
   submitMessage = (messageString) => {
     // on submitting the ChatInput form, send the message, add it to the list and reset the input
     const message = { name: this.state.name, message: messageString };
-    this.ws.send(JSON.stringify(message));
+    this.wss.send(JSON.stringify(message));
     this.addMessage(message);
   };
   render() {
@@ -48,7 +48,7 @@ class Chatroom extends Component {
         <NavBar />
         <div className="messIn">
           <ChatInput
-            ws={this.ws}
+            wss={this.wss}
             onSubmitMessage={(messageString) =>
               this.submitMessage(messageString)
             }
